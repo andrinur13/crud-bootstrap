d8d973ab748b81aab8b68678ea7c4cf043718dbb
andrinur13 <andribis13@gmail.com>
2020-01-12 15:42:36 +0700

    menambah tombol add

--- index.php
+++ index.php
@@ -1,19 +1,20 @@
-<?php  
+<?php
 
-    include "koneksi.php";
+include "koneksi.php";
 
-    $kueri = mysqli_query($conn, "SELECT * FROM buah");
-    $tampungbuah = [];
+$kueri = mysqli_query($conn, "SELECT * FROM buah");
+$tampungbuah = [];
 
-    while($row = mysqli_fetch_assoc($kueri)) {
-        $tampungbuah[] = $row;
-    }
+while ($row = mysqli_fetch_assoc($kueri)) {
+    $tampungbuah[] = $row;
+}
 
 ?>
 
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <title>Title</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
@@ -21,14 +22,18 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  </head>
-  <body>
+</head>
+
+<body>
     <br>
     <div class="container">
         <h1 class="text-center">Toko Buah</h1>
     </div>
     <br>
     <div class="container">
+        <a href="tambah.php" class="btn btn-success">Tambah Data</a>
+        <br>
+        <br>
         <table class="table">
             <thead>
                 <tr>
@@ -39,24 +44,25 @@
                 </tr>
             </thead>
             <tbody>
-                <?php foreach($tampungbuah as $buah) : ?>
+                <?php foreach ($tampungbuah as $buah) : ?>
 
-                <tr>
-                    <td> <?php echo $buah['id']; ?> </td>
-                    <td> <?php echo $buah['nama_buah']; ?> </td>
-                    <td> <?php echo $buah['harga_buah']; ?> </td>
-                    <td> <a href="edit.php?&id=<?=$buah['id']; ?>" class="btn btn-warning">Edit</a> <a href="hapus.php?&id=<?=$buah['id']; ?>" class="btn btn-danger">Hapus</a> </td>
-                </tr>
+                    <tr>
+                        <td> <?php echo $buah['id']; ?> </td>
+                        <td> <?php echo $buah['nama_buah']; ?> </td>
+                        <td> <?php echo $buah['harga_buah']; ?> </td>
+                        <td> <a href="edit.php?&id=<?= $buah['id']; ?>" class="btn btn-warning">Edit</a> <a href="hapus.php?&id=<?= $buah['id']; ?>" class="btn btn-danger">Hapus</a> </td>
+                    </tr>
 
                 <?php endforeach; ?>
             </tbody>
         </table>
     </div>
-      
+
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
+</body>
+
 </html>
