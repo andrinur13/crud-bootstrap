d8feb1c44931d47f5f36ffd09ba254bceadf8819
andrinur13 <andribis13@gmail.com>
2020-01-12 15:27:50 +0700

    tambah hapus php

--- hapus.php
+++ hapus.php
@@ -1,5 +1,11 @@
 <?php  
 
+    $id = $_GET['id'];
+    
+    include "koneksi.php";
 
+    $kueri_hapus = mysqli_query($conn, "DELETE FROM buah WHERE id = '$id' ");
+
+    header("Location:index.php");
 
 ?>
